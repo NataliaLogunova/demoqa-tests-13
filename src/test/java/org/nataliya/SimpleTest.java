66e0d4c329199ac091bbbba5fb01912ebd26ba08
NataliaLogunova <natali.elle@mail.ru>
2022-06-06 00:02:16 +0500

    tests added

---
+++ src/test/java/org/nataliya/SimpleTest.java
@@ -0,0 +1,32 @@
+package org.nataliya;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @BeforeEach
+    void openYaPage() {
+        Selenide.open("https://ya.ru");
+    }
+
+    @AfterEach
+    void close() {
+        WebDriverRunner.closeWindow();
+    }
+
+    @Test
+    void assertTest1(){
+        // ... поиск в новостях
+    }
+
+    @Test
+    void assertTest2(){
+        // ... поиск в картинках
+    }
+
+}
