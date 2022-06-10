900cfc25aa4541360d40d28cf05e77648103c83a
NataliaLogunova <natali.elle@mail.ru>
2022-06-10 05:25:47 +0500

    Practice form

--- src/test/java/org/nataliya/tests/RegistrationForm.java
+++ src/test/java/org/nataliya/tests/RegistrationForm.java
@@ -3,17 +3,12 @@ package org.nataliya.tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
-import java.time.Duration;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
-public class HomeWork {
+public class RegistrationForm {
 
     @BeforeAll
     static void setUp() {
